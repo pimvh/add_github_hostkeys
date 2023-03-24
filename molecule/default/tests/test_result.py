bb14d75d490f59ce02df62ed820d482297ef564c
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2023-03-24 20:20:09 +0100

    feat: rebase, implemented molecule for this repo
    fix: move to fqdn
    
    feat: add ansiblelint
    
    feat: fix lint issue, update minimum version
    
    feat: add yamllint, delete obsolete file
    
    fix: rolename was wrong in assert
    
    chore: lint file
    
    fix: more things to get molecule working
    
    fix: remove ansible-galaxy user handle from testing playbook
    
    fix: name of ansible-playbook
    
    fix: typo in argument spec
    
    fix: test whether passing become true fixes CI/CD
    
    fix: allow privilege escalation
    
    fix: disable fact gathering
    
    fix: broken ci
    
    fix: add cgroup
    
    fix: reintroduce fact gathering
    
    fix: simplify github action
    
    fix: fix github action
    
    fix: revert to basic workflow
    
    fix: try different approach
    
    fix: lint
    
    fix: lint
    
    fix: retry
    
    fix: edit image
    
    fix: simplify
    
    fix: issue in action
    
    fix: add prebuild image false
    
    fix: rename instance
    
    feat: tested molecule locally, now moving to Action
    
    fix: removed requirements, added package to test.yaml

---
+++ molecule/default/tests/test_result.py
@@ -0,0 +1,17 @@
+import testinfra
+
+
+def test_os_release(host):
+    """test host release for good measure"""
+
+    assert host.file("/etc/os-release").contains("Ubuntu")
+
+
+def test_known_hosts(host):
+    """test ssh ssh_known_hosts file has been created,
+    with the requested key types"""
+
+    assert host.file("/etc/ssh/ssh_known_hosts")
+
+    for key in ["ssh-rsa", "ssh-ed25519", "ecdsa-sha2-nistp256"]:
+        assert host.file("/etc/ssh/ssh_known_hosts").contains(f"github.com {key}")
